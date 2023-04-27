bb6a2b5d53f1709d4161ccbbd3eca9e9c4dfb2fe
attidack <attidack@gmail.com>
2023-04-27 16:25:56 -0600

    added add and deny friend requests

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -5,6 +5,9 @@ import { getServerSession } from 'next-auth'
 import { notFound } from 'next/navigation'
 import {  FC, ReactNode } from 'react'
 import Image from 'next/image'
+import SignOutButton from '@/components/SignOutButton'
+import FriendRequestsSidebarOption from '@/components/FriendRequestsSidebarOption'
+import { fetchRedis } from '@/helpers/redis'
 
 interface LayoutProps {
   children: ReactNode
@@ -30,6 +33,12 @@ const sidebarOptions: SidebarOption[] = [
 const Layout = async ({ children }: LayoutProps) => {
   const session = await getServerSession(authOptions)
   if (!session) notFound()
+
+  const unseenRequestCount = (await fetchRedis(
+      'smembers', 
+      `user:${session.user.id}:incoming_friend_requests`
+      ) as User[]
+    ).length
 
   return (
     <div className='w-full flex h-screen'>
@@ -68,7 +77,11 @@ const Layout = async ({ children }: LayoutProps) => {
                 })}
               </ul>
             </li>
-
+            <li>
+              <FriendRequestsSidebarOption 
+              sessionId={session.user.id} 
+              initalUnseenRequestCount={unseenRequestCount} />
+            </li>
             <li className='-mx-6 mt-auto flex items-center'>
                 <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
                   <div className='relative h-8 w-8 bg-gray-50'>
@@ -89,8 +102,8 @@ const Layout = async ({ children }: LayoutProps) => {
                       {session.user.email}
                     </span>
                   </div>
-
                 </div>
+                <SignOutButton className='h-full aspect-s'/>
             </li>
           </ul>
         </nav>
